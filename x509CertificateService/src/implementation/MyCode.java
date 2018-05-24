8138542d7f493819d2b07f3e074ee2d72d577b10
MrLaki5 <mimar1207@gmail.com>
2018-05-24 09:54:59 +0200

    load and reset KeyStore, bouncyCastle added

--- x509CertificateService/src/implementation/MyCode.java
+++ x509CertificateService/src/implementation/MyCode.java
@@ -1,15 +1,27 @@
 package implementation;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.Security;
 import java.util.Enumeration;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import code.GuiException;
 import x509.v3.CodeV3;
 
 public class MyCode extends CodeV3 {
+	
+	private static String LOCAL_KEY_STORE_PATH="/Users/milanlazarevic/Desktop/myStore.pkcs12";
+	private static String LOCAL_KEY_STORAGE_PASS="12345678";
+	
+	private KeyStore localKeyStore;
 
 	public MyCode(boolean[] algorithm_conf, boolean[] extensions_conf, boolean extensions_rules) throws GuiException {
 		super(algorithm_conf, extensions_conf, extensions_rules);
-		// TODO Auto-generated constructor stub
+		Security.addProvider(new BouncyCastleProvider());
 	}
 
 	@Override
@@ -86,8 +98,32 @@ public class MyCode extends CodeV3 {
 
 	@Override
 	public Enumeration<String> loadLocalKeystore() {
-		// TODO Auto-generated method stub
-		return null;
+		Enumeration<String> enumeration=null;
+		File file=new File(LOCAL_KEY_STORE_PATH);
+		FileInputStream inStream=null;
+		try {
+			localKeyStore=KeyStore.getInstance("PKCS12");	
+			if(file.exists() && file.isFile()) {
+				inStream= new FileInputStream(file);
+				localKeyStore.load(inStream, LOCAL_KEY_STORAGE_PASS.toCharArray());
+			}
+			else {
+				localKeyStore.load(null, LOCAL_KEY_STORAGE_PASS.toCharArray());
+			}
+			enumeration=localKeyStore.aliases();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(inStream!=null) {
+					inStream.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return enumeration;
 	}
 
 	@Override
@@ -98,8 +134,10 @@ public class MyCode extends CodeV3 {
 
 	@Override
 	public void resetLocalKeystore() {
-		// TODO Auto-generated method stub
-
+		File file=new File(LOCAL_KEY_STORE_PATH);
+		if(file.exists()) {
+			file.delete();
+		}
 	}
 
 	@Override
